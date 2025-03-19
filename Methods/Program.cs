0c248a15803f5c458e5b2c279a5e37f755e69150
ALIJAMIL-dev <alijamil@outlook.com.tr>
2025-03-19 20:09:32 +0300

    Methods

---
+++ Methods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Add1();
+            Add1();
+            Add1();
+            var result = Add(10, 25);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        static void Add1()
+        {
+            Console.WriteLine("Added!");
+        }
+        static int Add(int number1 = 10, int number2 = 1)
+        {
+            var result = number1 + number2;
+            return result;
+        }
+        
+    }
+}
