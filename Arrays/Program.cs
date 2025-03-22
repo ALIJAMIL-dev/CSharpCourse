e95895836f04eb1a180fc2ee251e42363f045269
ALIJAMIL-dev <alijamil@outlook.com.tr>
2025-03-22 17:52:44 +0300

    Arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Arrays 
+            // string student1 = "John";
+            // Ali is my name
+            // string student2 = "Ali";
+            // string student3 = "Jack";
+            //string[] students = new string[3];
+            //students[0] = "John";
+            //students[1] = "Ali";
+            //students[2] = "Jack";
+
+            //string[] students2 = {"Ali", "John", "Jack"}; // or string[] students2 =
+            //// new [] {"Ali", "John", "Jack"};
+
+            //foreach (var student in students)
+            //{
+            //    Console.WriteLine(student);
+            //}
+
+            //foreach (var student1 in students2)
+            //{
+            //    Console.WriteLine(student1);
+            //}
+
+            string[,] TurkeyRegions = new string[5, 3]
+            {
+                {"Istanbul", "Izmit", "Balikesir"},
+                {"Ankara", "Konya", "Kirikkale"},
+                {"Antalya", "Adana", "Hatay"},
+                {"Rize", "Trabzon", "Samsun"},
+                {"Erzurum", "Kars", "Agri"}
+            };
+
+                for (int i = 0; i <= TurkeyRegions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= TurkeyRegions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(TurkeyRegions[i, j]);
+                }
+                Console.WriteLine("*************************");
+            }
+        
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
