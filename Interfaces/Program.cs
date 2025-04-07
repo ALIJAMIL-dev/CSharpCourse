d45cded92096d9d0022cac758adf4ee5bf2fc97e
ALI JAMIL <alijamil@outlook.com.tr>
2025-04-07 13:33:08 +0300

    Applying

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -10,28 +10,45 @@ namespace Interfaces
     {
         static void Main(string[] args)
         {
+            PersonManager manager = new PersonManager();
+            Customer customer = new Customer
+            {
+                ID = 1,
+                FirstName = "Engin",
+                LastName = "Demiroğ",
+                Address = "Ankara"
+            };
+            manager.Add(customer);
+            Console.ReadLine();
         }
         interface IPerson
         {
             int ID { get; set; }
-            int FirstName { get; set; }
-            int LastName { get; set; }
+            string FirstName { get; set; }
+            string LastName { get; set; }
 
         }
         class Customer : IPerson
         {
             public int ID { get; set; }
-            public int FirstName { get; set; }
-            public int LastName { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
             public string Address { get; set; }
         }
         class Student : IPerson
         {
             public int ID { get; set; }
-            public int FirstName { get; set; }
-            public int LastName { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
             public string Departmant { get; set; }
 
+        }
+        class PersonManager
+        {
+            public void Add(Customer customer)
+            {
+                Console.WriteLine(customer.FirstName);
+            }
         }
     }
 }
