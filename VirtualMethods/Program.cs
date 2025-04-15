395e539c05633852e0ca1f1fcb3805fd858e9ad8
ALI JAMIL <alijamil@outlook.com.tr>
2025-04-15 16:35:27 +0300

    Virtual Methods

---
+++ VirtualMethods/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        class Database
+        {
+            public virtual void Add()
+            {
+                Console.WriteLine("Added by default!");
+            }
+            public virtual void Delete()
+            {
+                Console.WriteLine("Delete by default!");
+            }
+        }
+        class SqlServer : Database
+        {
+            public override void Add()
+            {
+                Console.WriteLine("Added by SQL");
+                //base.Add();
+            }
+        }
+        class MySQL : Database
+        {
+
+        }
+    }
+}
