1cceb7db65aa6c9de15bc7fc661ba6c9abc2af49
ALI JAMIL <alijamil@outlook.com.tr>
2025-04-16 14:54:44 +0300

    Abstract Classes

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    // This is Abstract Class 
+    abstract class AbstractClass
+    {
+        protected AbstractClass() 
+        {
+            Console.WriteLine("Abstract Class Constructor");
+        }
+    }
+    // Summarize of Abstract Class is:
+    // 1.Abstract Classes are using with Inheritance(Virtual Methods)
+}
