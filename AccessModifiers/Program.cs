098f20e1637bb53a6fc45fd033739176563f8f08
ALI JAMIL <alijamil@outlook.com.tr>
2025-05-13 16:24:56 +0300

    Access Modifiers

---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Customer
+    {
+        protected int Id1;
+  
+        public int id;
+    }
+    class Student
+    {
+        public void Save()
+        {
+            Customer customer = new Customer();
+            // customer.id;
+        }
+    }
+    public class Course
+    {
+        public int Id;
+        public int Name;
+        public void Save()
+        {
+            Console.WriteLine("Saved");
+        }
+    }
+}
