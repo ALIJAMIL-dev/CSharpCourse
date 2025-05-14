6183a6bbec1a54f40802b66667a0deb976d3efd0
ALI JAMIL <alijamil@outlook.com.tr>
2025-05-14 15:38:31 +0300

    References and Value Types

---
+++ ReferenceAndValueTypes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceAndValueTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 19;
+            num1 = num2;
+            num2 = 39;
+            Console.WriteLine(num2);
+
+            Console.ReadLine();
+
+        }
+    }
+}
