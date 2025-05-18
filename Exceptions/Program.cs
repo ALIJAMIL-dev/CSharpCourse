a38fb9cc5dbf06fb6762ee1e6cc3fa1fc4a644cb
ALI JAMIL <alijamil@outlook.com.tr>
2025-05-18 09:41:25 +0300

    Exception Handling

---
+++ Exceptions/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ExceptionsIntro();
+
+            // Find();
+
+            try
+            {
+                Find();
+            }
+            catch (RecordNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Find()
+        {
+            List<string> students1 = new List<string>
+            {
+                "Engin", "Derin", "Salih"
+            };
+
+            if (!students1.Contains("Ahmet"))
+            {
+                throw new RecordNotFoundException("RecordNotFound!");
+            }
+            else
+            {
+                Console.WriteLine("Record Found");
+            }
+        }
+
+        private static void ExceptionsIntro()
+        {
+            try
+            {
+                string[] students = new string[3]
+                {
+                "Engin", "Derin", "Salih"
+                };
+                students[3] = "Ahmet";
+            }
+            catch (IndexOutOfRangeException ex1)
+            {
+                Console.WriteLine(ex1.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception {0}", ex.Message);
+            }
+        }
+    }
+}
