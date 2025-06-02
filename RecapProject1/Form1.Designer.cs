610e516cfa63a63ce8bb6aaa1c5ad9e2e07652df
ALI JAMIL <alijamil@outlook.com.tr>
2025-06-02 13:08:36 +0300

    Project 1 - Design Form1

---
+++ RecapProject1/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace RecapProject1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxCategory = new System.Windows.Forms.GroupBox();
+            this.cbxCategory = new System.Windows.Forms.ComboBox();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.gbxSearch = new System.Windows.Forms.GroupBox();
+            this.tbxSearch = new System.Windows.Forms.TextBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.gbxCategory.SuspendLayout();
+            this.gbxSearch.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbxCategory
+            // 
+            this.gbxCategory.Controls.Add(this.cbxCategory);
+            this.gbxCategory.Controls.Add(this.lblCategory);
+            this.gbxCategory.Location = new System.Drawing.Point(12, 12);
+            this.gbxCategory.Name = "gbxCategory";
+            this.gbxCategory.Size = new System.Drawing.Size(1028, 90);
+            this.gbxCategory.TabIndex = 0;
+            this.gbxCategory.TabStop = false;
+            this.gbxCategory.Text = "Choose a category";
+            // 
+            // cbxCategory
+            // 
+            this.cbxCategory.FormattingEnabled = true;
+            this.cbxCategory.Location = new System.Drawing.Point(71, 37);
+            this.cbxCategory.Name = "cbxCategory";
+            this.cbxCategory.Size = new System.Drawing.Size(361, 24);
+            this.cbxCategory.TabIndex = 1;
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Location = new System.Drawing.Point(7, 37);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(65, 17);
+            this.lblCategory.TabIndex = 0;
+            this.lblCategory.Text = "Category";
+            // 
+            // gbxSearch
+            // 
+            this.gbxSearch.Controls.Add(this.tbxSearch);
+            this.gbxSearch.Controls.Add(this.lblSearch);
+            this.gbxSearch.Location = new System.Drawing.Point(12, 108);
+            this.gbxSearch.Name = "gbxSearch";
+            this.gbxSearch.Size = new System.Drawing.Size(1028, 87);
+            this.gbxSearch.TabIndex = 1;
+            this.gbxSearch.TabStop = false;
+            this.gbxSearch.Text = "Search a Product";
+            // 
+            // tbxSearch
+            // 
+            this.tbxSearch.Location = new System.Drawing.Point(71, 36);
+            this.tbxSearch.Name = "tbxSearch";
+            this.tbxSearch.Size = new System.Drawing.Size(361, 24);
+            this.tbxSearch.TabIndex = 1;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(10, 44);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(50, 17);
+            this.lblSearch.TabIndex = 0;
+            this.lblSearch.Text = "Search";
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Location = new System.Drawing.Point(12, 201);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.RowHeadersWidth = 51;
+            this.dgvProducts.RowTemplate.Height = 26;
+            this.dgvProducts.Size = new System.Drawing.Size(1028, 254);
+            this.dgvProducts.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1057, 664);
+            this.Controls.Add(this.dgvProducts);
+            this.Controls.Add(this.gbxSearch);
+            this.Controls.Add(this.gbxCategory);
+            this.Name = "Form1";
+            this.Text = "Products";
+            this.gbxCategory.ResumeLayout(false);
+            this.gbxCategory.PerformLayout();
+            this.gbxSearch.ResumeLayout(false);
+            this.gbxSearch.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxCategory;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.ComboBox cbxCategory;
+        private System.Windows.Forms.GroupBox gbxSearch;
+        private System.Windows.Forms.TextBox tbxSearch;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.DataGridView dgvProducts;
+    }
+}
+
