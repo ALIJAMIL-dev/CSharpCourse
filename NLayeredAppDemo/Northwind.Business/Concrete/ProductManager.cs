80b33d182dab22afc95ce06044516e8d9bc9b894
ALI JAMIL <alijamil@outlook.com.tr>
2025-06-04 14:12:19 +0300

    Project 2 - N Layered App Demo

---
+++ NLayeredAppDemo/Northwind.Business/Concrete/ProductManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Northwind.Entities.Concrete;
+using Northwind.DataAccess.Concrete;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Business.Concrete
+{
+    public class ProductManager
+    {
+        ProductDal _productDal = new ProductDal();
+        public List<Product> GetAll()
+        {
+            //Business Code
+            return _productDal.GetAll();
+        }
+    }
+}
