c5c36621bda95fb71e529cef888aebfd9f55e8e7
ALI JAMIL <alijamil@outlook.com.tr>
2025-07-02 08:56:15 +0300

    Design Patterns - Singleton + Thread Safe

---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManager = CustomerManager.CreateAsSingleton();
+            customerManager.AddCustomer("John Doe");
+        }
+    }
+
+    class CustomerManager
+    {
+        static object _lockObject = new object();
+        private static CustomerManager _customerManager;
+        public CustomerManager()
+        {
+
+        }
+        public static CustomerManager CreateAsSingleton()
+        {
+            lock (_lockObject)
+            {
+                if (_customerManager == null)
+                {
+                    _customerManager = new CustomerManager();
+                }
+            }
+            return _customerManager;
+        }
+        public void AddCustomer(string customerName)
+        {
+            Console.WriteLine("Customer added: " + customerName);
+        }
+    }
+}
