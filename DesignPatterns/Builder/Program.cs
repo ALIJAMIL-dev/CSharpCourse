0fefbb69c8ef3988bd9b33a7b2b1a0832821f9f0
ALI JAMIL <alijamil@outlook.com.tr>
2025-08-02 21:43:25 +0300

    Design Patterns Builder one that GEt Sus in AmongUs irl as an airsoft player who a sniper that known as "Motherf*cker"

---
+++ DesignPatterns/Builder/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductDirector director = new ProductDirector();
+            var builder = new NewCustomerProductBuilder();
+            director.GenerateProduct(builder);
+            var model = builder.GetModel();
+
+            Console.WriteLine($"Product: {model.ProductName}, Price: {model.UnitPrice:C}, Discounted Price: {model.DiscountedPrice:C}, Discount Applied: {model.DiscountApplied}");
+        }
+    }
+
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal DiscountedPrice { get; set; }
+        public bool DiscountApplied { get; set; }
+    }
+
+    abstract class ProductBuilder
+    {
+        public abstract void GetProductData();
+        public abstract void ApplyDiscount();
+        public abstract ProductViewModel GetModel();
+    }
+
+    class NewCustomerProductBuilder : ProductBuilder
+    {
+        ProductViewModel Model = new ProductViewModel();
+
+        public override void ApplyDiscount()
+        {
+            Model.DiscountApplied = true;
+            Model.DiscountedPrice = Model.UnitPrice * (decimal)0.90;
+        }
+
+        public override void GetProductData()
+        {
+            Model.Id = 1;
+            Model.CategoryName = "Electronics";
+            Model.ProductName = "Smartphone";
+            Model.UnitPrice = 699;
+        }
+
+        public override ProductViewModel GetModel()
+        {
+            return Model;
+        }
+    }
+
+    class OldCustomerProductBuilder : ProductBuilder
+    {
+        ProductViewModel Model = new ProductViewModel();
+
+        public override void ApplyDiscount()
+        {
+            Model.DiscountApplied = true;
+            Model.DiscountedPrice = Model.UnitPrice;
+        }
+
+        public override void GetProductData()
+        {
+            Model.Id = 1;
+            Model.CategoryName = "Electronics";
+            Model.ProductName = "Smartphone";
+            Model.UnitPrice = 699;
+        }
+
+        public override ProductViewModel GetModel()
+        {
+            return Model;
+        }
+    }
+
+    class ProductDirector
+    {
+        public void GenerateProduct(ProductBuilder builder)
+        {
+            builder.GetProductData();
+            builder.ApplyDiscount();
+        }
+    }
+}
---
+++ DesignPatterns/Prototype/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer
+            {
+                FirstName = "Me",
+                LastName = "You",
+                City = "He",
+                Id = 1
+            };
+
+            Customer customerClone = (Customer)customer.Clone();
+            customerClone.FirstName = "The";
+            customerClone.LastName = "GOAT";
+            customerClone.City = "'Cristiano Ronaldo 7'";
+
+            Console.WriteLine($"Customer: {customer.FirstName} {customer.LastName} {customer.City}");
+            Console.WriteLine($"The truth: {customerClone.FirstName} {customerClone.LastName} is {customerClone.City}");
+
+            Console.ReadLine();
+        }
+    }
+
+    public abstract class Person
+    {
+        public abstract Person Clone();
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+
+    public class Customer : Person
+    {
+        public string City { get; set; }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+
+    public class Employee : Person
+    {
+        public decimal Salary { get; set; }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+}
